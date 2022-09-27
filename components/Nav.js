a2ca6fc86524ee543ddfb1930c19139381ff1bc6
Wayne Howlett <93297336+WHowlett@users.noreply.github.com>
2022-09-27 16:47:01 -0300

    Update Nav.js
    update A to Link

--- components/Nav.js
+++ components/Nav.js
@@ -1,24 +1,26 @@
+import Link from 'next/link';
+
 export default function Navbar ({children}) {
     return (
            <div>
             <nav className=" navbar navbar-dark navbar-expand-md fixed-top">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Howlett's Family Tree
-                    </a>
+                    <Link className="navbar-brand" href="/">Howlett's Family Tree
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarCollapse">
                         <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                            <li className="nav-item"><a href="/" className="nav-link active px-2">Home</a></li>
-                            <li className="nav-item"><a href="/about" className="nav-link active px-2">About</a></li>
+                            <li className="nav-item"><Link href="/" className="nav-link active px-2">Home</Link></li>
+                            <li className="nav-item"><Link href="/about" className="nav-link active px-2">About</Link></li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown" aria-expanded="false">Careers</a>
+                                <Link className="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown" aria-expanded="false">Careers</Link>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/people/1">Wayne</a></li>
-                                    <li><a className="dropdown-item" href="/people/2">Lara</a></li>
-                                    <li><a className="dropdown-item" href="/people/3">Alex</a></li>
-                                    <li><a className="dropdown-item" href="/people/4">Daniel</a><li>
+                                    <li><Link className="dropdown-item" href="/people/1">Wayne</Link></li>
+                                    <li><Link className="dropdown-item" href="/people/2">Lara</Link></li>
+                                    <li><Link className="dropdown-item" href="/people/3">Alex</Link></li>
+                                    <li><Link className="dropdown-item" href="/people/4">Daniel</Link><li>
                                 </ul>
                             </li>
                         </ul>
