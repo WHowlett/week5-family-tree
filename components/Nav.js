db0cb0019260cc50f84e26ddf93458280cfb4ad5
Wayne Howlett <93297336+WHowlett@users.noreply.github.com>
2022-09-27 16:43:39 -0300

    Update Nav.js
    update

--- components/Nav.js
+++ components/Nav.js
@@ -1,35 +1,30 @@
 export default function Navbar ({children}) {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Howlett's Family Tree</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Person List
-          </a>
-          <ul class="dropdown-menu">
-            <li><a className="dropdown-item" href="/people/1">Wayne</a></li>
-            <li><a className="dropdown-item" href="/people/2">Lara</a></li>
-            <li><a className="dropdown-item" href="/people/3">Alex</a></li>
-            <li><a className="dropdown-item" href="/people/4">Daniel</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+           <div>
+            <nav className=" navbar navbar-dark navbar-expand-md fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Howlett's Family Tree
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                            <li className="nav-item"><a href="/" className="nav-link active px-2">Home</a></li>
+                            <li className="nav-item"><a href="/about" className="nav-link active px-2">About</a></li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown" aria-expanded="false">Careers</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="/people/1">Wayne</a></li>
+                                    <li><a className="dropdown-item" href="/people/2">Lara</a></li>
+                                    <li><a className="dropdown-item" href="/people/3">Alex</a></li>
+                                    <li><a className="dropdown-item" href="/people/4">Daniel</a><li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
